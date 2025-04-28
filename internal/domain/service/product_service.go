554618b58d41dd3aeb19f5363e79eae00fa54a02
Isaque Duarte <isaqueduarte17@gmail.com>
2025-04-28 12:16:35 -0300

    feat<product>: finish product service

--- internal/domain/service/product_service.go
+++ internal/domain/service/product_service.go
@@ -1,19 +1,30 @@
 package service
 
 import (
+	productdto "github.com/harry-fruit/ddd-go/internal/application/dto/product"
 	productusecase "github.com/harry-fruit/ddd-go/internal/application/use_case/product"
-	"github.com/harry-fruit/ddd-go/internal/domain/entity"
 	abstractrepository "github.com/harry-fruit/ddd-go/internal/domain/repository"
+	pagination "github.com/harry-fruit/ddd-go/pkg"
+
+	"github.com/harry-fruit/ddd-go/internal/domain/entity"
 )
 
 type ProductService struct {
-	productRepository    abstractrepository.ProductRepository
-	createProductUseCase *productusecase.CreateProductUseCase
+	productRepository     abstractrepository.ProductRepository
+	createProductUseCase  *productusecase.CreateProductUseCase
+	getProductByIdUseCase *productusecase.GetProductByIdProductUseCase
+	getProductsUseCase    *productusecase.GetProductsUseCase
+	updateProductUseCase  *productusecase.UpdateProductUseCase
+	deleteProductUseCase  *productusecase.DeleteProductUseCase
 }
 
 func NewProductService(
 	productRepository abstractrepository.ProductRepository,
 	createProductUseCase *productusecase.CreateProductUseCase,
+	getProductByIdUseCase *productusecase.GetProductByIdProductUseCase,
+	getProductsUseCase *productusecase.GetProductsUseCase,
+	updateProductUseCase *productusecase.UpdateProductUseCase,
+	deleteProductUseCase *productusecase.DeleteProductUseCase,
 ) *ProductService {
 	return &ProductService{
 		productRepository:    productRepository,
@@ -21,6 +32,22 @@ func NewProductService(
 	}
 }
 
-func (ps *ProductService) CreateProduct(product *entity.Product) (bool, error) {
-	return ps.createProductUseCase.Execute(product)
+func (ps *ProductService) CreateProduct(params productdto.CreateProductDTO) (bool, error) {
+	return ps.createProductUseCase.Execute(params)
+}
+
+func (ps *ProductService) GetProductById(id int) (*entity.Product, error) {
+	return ps.getProductByIdUseCase.Execute(id)
+}
+
+func (ps *ProductService) GetProducts() (*pagination.PaginationResult[[]*entity.Product], error) {
+	return ps.getProductsUseCase.Execute(nil)
+}
+
+func (ps *ProductService) UpdateProduct(params productdto.UpdateProductDTO) (bool, error) {
+	return ps.updateProductUseCase.Execute(params)
+}
+
+func (ps *ProductService) DeleteProduct(id int) (bool, error) {
+	return ps.deleteProductUseCase.Execute(id)
 }
