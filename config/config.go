6dd4adfc4eb29d88fecd267a2fd7b0e54aedccc1
Isaque Duarte <isaqueduarte17@gmail.com>
2025-05-03 19:27:34 -0300

    improv<config>: add db sql parameters

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -12,18 +13,42 @@ type Config struct {
 	DatabaseURL string
 	Environment string
 	LogLevel    string
+	DbHost      string
+	DbPort      string
+	DbUser      string
+	DbPassword  string
+	DbName      string
+	DbSslmode   string
 }
 
 func LoadConfig() (*Config, error) {
-	if err := godotenv.Load(); err != nil {
+	env, err := getRunMode()
+	if err != nil {
+		return nil, err
+	}
+
+	fileName := getEnvFileName(*env)
+
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("Current directory:", dir)
+
+	if err := godotenv.Load(fileName); err != nil {
 		return nil, fmt.Errorf("error loading .env file: %v", err)
 	}
 
 	return &Config{
+		Environment: *env,
 		ServerPort:  getEnv("API_PORT", "3001"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"),
-		Environment: getEnv("ENVIRONMENT", "development"),
 		LogLevel:    getEnv("LOG_LEVEL", "info"),
+		DbHost:      getEnv("DB_HOST", ""),
+		DbPort:      getEnv("DB_PORT", ""),
+		DbUser:      getEnv("DB_USER", ""),
+		DbPassword:  getEnv("DB_PASSWORD", ""),
+		DbName:      getEnv("DB_NAME", ""),
+		DbSslmode:   getEnv("DB_SSLMODE", ""),
 	}, nil
 }
 
@@ -31,6 +56,22 @@ func getEnv(key string, defaultValue string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
-
 	return defaultValue
+}
+
+func getRunMode() (result *string, err error) {
+	envFlag := flag.String("env", "", "Environment")
+
+	flag.Parse()
+
+	env := *envFlag
+	if env == "" {
+		return nil, fmt.Errorf("no environment specified")
+	}
+
+	return &env, nil
+}
+
+func getEnvFileName(env string) string {
+	return fmt.Sprintf(".env.%s", env)
 }
