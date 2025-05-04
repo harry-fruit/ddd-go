8fd69a7eb563e777435a2dfbe924bc1527317a9d
Isaque Duarte <isaqueduarte17@gmail.com>
2025-05-04 17:28:51 -0300

    improv<infrastructure>: add constructor for ProductHandler

--- internal/infrastructure/api/http/handler/product_handler.go
+++ internal/infrastructure/api/http/handler/product_handler.go
@@ -2,20 +2,20 @@ package httphandler
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/harry-fruit/ddd-go/internal/domain/service"
+	httpserver "github.com/harry-fruit/ddd-go/internal/infrastructure/api/http"
 )
 
 type ProductHandler struct {
-	service *service.ProductService
+	service       *service.ProductService
+	routeHandlers []*httpserver.RouteHandler
 }
 
-func NewProductHandler(service *service.ProductService) *ProductHandler {
-	productHandler := &ProductHandler{
-		service: service,
-	}
-	return productHandler
+func (ph *ProductHandler) GetRoutes() []*httpserver.RouteHandler {
+	return ph.routeHandlers
 }
 
 func (ph *ProductHandler) CreateProduct(c *fiber.Ctx) error {
@@ -31,8 +31,8 @@ func (ph *ProductHandler) GetProducts(c *fiber.Ctx) error {
 }
 
 func (ph *ProductHandler) GetProductById(c *fiber.Ctx) error {
-	fmt.Println(c.Method())
-	return c.SendString("Hello, World!")
+	fmt.Println("DI Worked just fine")
+	return nil
 }
 
 func (ph *ProductHandler) UpdateProduct(c *fiber.Ctx) error {
@@ -43,4 +43,22 @@ func (ph *ProductHandler) UpdateProduct(c *fiber.Ctx) error {
 func (ph *ProductHandler) DeleteProduct(c *fiber.Ctx) error {
 	fmt.Println(c.Method())
 	return c.SendString("Hello, World!")
+}
+
+func NewProductHandler(service *service.ProductService) httpserver.Handler {
+	productHandler := &ProductHandler{
+		service: service,
+	}
+
+	fmt.Println("NewProductHandler")
+
+	productHandler.routeHandlers = []*httpserver.RouteHandler{
+		httpserver.NewRouteHandler(http.MethodGet, "/products", productHandler.GetProducts),
+		httpserver.NewRouteHandler(http.MethodPost, "/products", productHandler.CreateProduct),
+		httpserver.NewRouteHandler(http.MethodGet, "/products/:id", productHandler.GetProductById),
+		httpserver.NewRouteHandler(http.MethodPatch, "/products/:id", productHandler.UpdateProduct),
+		httpserver.NewRouteHandler(http.MethodDelete, "/products/:id", productHandler.DeleteProduct),
+	}
+
+	return productHandler
 }
