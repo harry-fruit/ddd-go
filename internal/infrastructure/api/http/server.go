08c514f3dd359f142390f5ca57a51886e7ceea09
Isaque Duarte <isaqueduarte17@gmail.com>
2025-05-08 18:32:13 -0300

    improv<rest api>: adding fiber + swagger into the project

--- internal/infrastructure/api/http/server.go
+++ internal/infrastructure/api/http/server.go
@@ -5,38 +5,52 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 	"github.com/harry-fruit/ddd-go/config"
-	"go.uber.org/dig"
+	sharedinfra "github.com/harry-fruit/ddd-go/internal/shared/infrastructure"
+
+	_ "github.com/harry-fruit/ddd-go/docs"
 )
 
 type HTTPServer struct {
-	port     string
-	Handlers []Handler
+	port   string
+	Routes []sharedinfra.Route
 }
 
-type HandlersIn struct {
-	dig.In
-	Handlers []Handler `group:"handlers"`
-}
-
-func NewHTTPServer(h HandlersIn, config *config.Config) *HTTPServer {
+func NewHTTPServer(routes sharedinfra.RoutesIn, config *config.Config) *HTTPServer {
 	return &HTTPServer{
-		port:     fmt.Sprintf(":%s", config.ServerPort),
-		Handlers: h.Handlers,
+		port:   fmt.Sprintf(":%s", config.ServerPort),
+		Routes: routes.Routes,
 	}
 }
 
-func (s *HTTPServer) Start(container *dig.Container) {
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /api/v1
+func (s *HTTPServer) Start() {
+
 	app := fiber.New(fiber.Config{
 		AppName: "ddd-go",
 	})
 
-	container.Invoke(RegisterAllRoutes)
-
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
 
-	RegisterAllRoutes(v1, s.Handlers)
+	app.Get("/swagger/*", swagger.New(swagger.Config{
+		URL:          "/swagger/doc.json", // or where swag outputs your spec
+		DocExpansion: "none",
+	}))
+
+	for _, route := range s.Routes {
+		route.Register(v1)
+	}
 
 	e := app.Listen(s.port)
 
