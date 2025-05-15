688da04808861bbef28e10951e0e6a0273a35657
Isaque Duarte <isaqueduarte17@gmail.com>
2025-05-14 22:46:29 -0300

    improv<product>:  add ToDomain func in CreateDTO

--- internal/application/dto/product/create_product_dto.go
+++ internal/application/dto/product/create_product_dto.go
@@ -1,10 +1,18 @@
 package productdto
 
+import (
+	"github.com/harry-fruit/ddd-go/internal/domain/entity"
+)
+
 type CreateProductDTO struct {
 	UniqueKey      string   `json:"unique_key" validate:"required,min=3,max=150"`
 	Name           string   `json:"name" validate:"required,min=3,max=150"`
 	Description    *string  `json:"description,omitempty"`
 	Price          float64  `json:"price" validate:"required"`
-	RemainingStock int      `json:"remaining_stock" validate:"required"`
+	RemainingStock *int     `json:"remaining_stock" validate:"required"`
 	Rating         *float64 `json:"rating,omitempty"`
+}
+
+func (dto *CreateProductDTO) ToDomain() *entity.Product {
+	return entity.NewProduct(dto.UniqueKey, dto.Name, dto.Description, dto.Price, dto.RemainingStock, dto.Rating)
 }
